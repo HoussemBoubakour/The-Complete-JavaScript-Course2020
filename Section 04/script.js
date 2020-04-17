51e2baf9679f9da0efb9ecde79774a46cad7ab46
HoussemBoubakour <houssemboubakour@gmail.com>
2020-04-17 07:58:25 -0400

    edit 04/17/2020

--- Section 04/script.js
+++ Section 04/script.js
@@ -1,4 +1,4 @@
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, previous, max_score;
 
 init();
 
@@ -8,21 +8,38 @@ init();
 
 document.querySelector('#roll-dice-btn').addEventListener('click', function(){
    if (gamePlaying){
-   
-            var dice = Math.floor(Math.random()*6) + 1;
 
-        diceDOM = document.querySelector('.img-dice');
+            
+            var dice_1 = Math.floor(Math.random()*6) + 1;
+            var dice_2 = Math.floor(Math.random()*6) + 1;
 
-        diceDOM.style.visibility = 'visible';
-        diceDOM.src = 'dice-'+dice+'.png';
+            console.log(previous);
 
-        if (dice!==1){
+        diceDOM_1 = document.querySelector('#dice1');
+        diceDOM_2 = document.querySelector('#dice2');
 
-            roundScore += dice;
+        diceDOM_1.style.visibility = 'visible';
+        diceDOM_1.src = 'dice-'+dice_1+'.png';
+
+        diceDOM_2.style.visibility = 'visible';
+        diceDOM_2.src = 'dice-'+dice_2+'.png';
+
+        if (previous === [6,6] && (dice_1 === 6 && dice_2 === 6)){
+                
+            scores[activePlayer-1] = 0;
+            document.querySelector('#player-total' + activePlayer).textContent = scores[activePlayer-1];
+            nextPlayer();
+
+        }else if (dice_1 !== 1 && dice_2 !== 1){
+                      
+            roundScore = roundScore+ dice_1 + dice_2;
+            previous = [dice_1,dice_2];
             document.querySelector ('#current-' + activePlayer).textContent = roundScore;
+            console.log(dice_1, dice_2);
+            
 
         }else{
-                nextPlayer();
+            nextPlayer();
         }
     };
 
@@ -35,13 +52,18 @@ document.getElementById('hold-btn').addEventListener('click', function(){
 
 
 
-    if(scores[activePlayer-1]>= 100){
+    if(scores[activePlayer-1]>= max_score){
             document.querySelector('#name-'+activePlayer).textContent = 'WINNER!';
-            document.querySelector ('.img-dice').style.visibility = 'hidden';
+
+            document.querySelector ('#dice1').style.visibility = 'hidden';
+            document.querySelector ('#dice2').style.visibility = 'hidden';
+            
             document.querySelector ('.CURRENT1').style.visibility = 'hidden';
             document.querySelector ('.CURRENT2').style.visibility = 'hidden';
+            
             document.querySelector('.panel-player'+ activePlayer).classList.add('winner');
             document.querySelector('.panel-player'+ activePlayer).classList.remove('active');
+            
             gamePlaying = false;
 
 
@@ -64,11 +86,17 @@ function nextPlayer(){
     document.querySelector('.panel-player1').classList.toggle('active');
     document.querySelector('.panel-player2').classList.toggle('active');
 
-    document.querySelector ('.img-dice').style.visibility = 'hidden';
+    document.querySelector ('#dice1').style.visibility = 'hidden';
+    document.querySelector ('#dice2').style.visibility = 'hidden';
 };
 
 
-document.querySelector('#new-game').addEventListener('click', init);
+document.querySelector('#new-game').addEventListener('click', function(){
+
+    document.querySelector('#yourScore').style.display = 'block';
+    init();
+
+});
 
 function init(){
         scores = [0,0];
@@ -76,7 +104,8 @@ function init(){
         activePlayer = 1;
         gamePlaying = true;
 
-    document.querySelector ('.img-dice').style.visibility = 'hidden';
+    document.querySelector ('#dice1').style.visibility = 'hidden';
+    document.querySelector ('#dice2').style.visibility = 'hidden';
     
     document.getElementById('current-1').textContent = '0';
     document.getElementById('current-2').textContent = '0';
@@ -99,3 +128,23 @@ function init(){
     document.querySelector('#name-1').textContent = 'PLAYER 1';
     document.querySelector('#name-2').textContent = 'PLAYER 2';
 };
+
+
+document.getElementById('ok').addEventListener('click', function(){
+
+    max_score = document.querySelector('.set_score').value;
+
+    document.querySelector('#yourScore').style.display = 'none';
+
+})
+
+
+document.getElementById('close').addEventListener('click', function(){
+    document.querySelector('.container1').style.display = 'none';
+
+});
+
+document.getElementById('help').addEventListener('click', function(){
+    document.querySelector('.container1').style.display = 'block';
+});
+
