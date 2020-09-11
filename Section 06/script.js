d2dfe77f1290282df160c73d3a837516a92103ff
HoussemBoubakour <houssemboubakour@gmail.com>
2020-09-11 19:35:11 -0400

    Edit 09/11

--- Section 06/script.js
+++ Section 06/script.js
@@ -1,26 +1,67 @@
+
+
+
+// BUDGET CONTROLLER
 var budgetController = (function(){
-    var x = 23;
+   
 
-    var add = function(a){
-        return x+a;
-    }
 
-return {
-        publicTest : function(b){
-            console.log(add(b));
-            }
-        } ;
+
 })();
 
+
+
+
+// UI CONTROLLER
 var UIController = (function(){
 
-
-
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    return {
+        getInput : function(){
+            return{
+                type : document.querySelector(DOMStrings.inputType).value, // inc or exp
+                description : document.querySelector(DOMStrings.inputDescription).value,
+                value : document.querySelector(DOMStrings.inputValue).value
+            };  
+        },
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    };
 })();
 
 
-var controller = (function(){
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCrtl,UICrtl){
+
+    var DOM = UIController.getDOMStrings();
+
+    var ctrlAddItem = function(){
+        // 1. Get the field input data
+        var input = UIController.getInput();
+        console.log(input);
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the list
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress',function(event){
+            if(event.keyCode === 13 || event.which === 13){
+
+                ctrlAddItem();
+
+            }
+    });
 
 
-    
-})();
+})(budgetController,UIController);
+ 
