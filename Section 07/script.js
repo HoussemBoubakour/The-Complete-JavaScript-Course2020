fc6b8cd5fcb97a6d2fda9ff76040191e293e442b
HoussemBoubakour <houssemboubakour@gmail.com>
2020-10-22 19:55:37 -0400

    Edit 10/22

--- Section 07/script.js
+++ Section 07/script.js
@@ -181,16 +181,72 @@ const[age2 , retirement] = calculateAgeRetirement(1990);
 console.log(age2);
 console.log(retirement);
 
-*/
+
 
 
 // Arrays In ES6
 
+const boxes = document.querySelectorAll('.box');
+
+//ES5
+
+var boxArray5 = Array.prototype.slice.call(boxes);
+
+boxArray5.forEach(function(cur){
+    cur.style.backgroundColor = 'dodgerblue';
+});
 
 
 
 
+//ES6
+const boxArray6 = Array.from(boxes);
+boxArray6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+//ES5 
+
+for (var i = 0; i< boxArray5.length; i++){
+    if (boxArray5[i].className === 'box blue'){
+        continue;
+    }
+    boxArray5[i].textContent = 'I changed to blue !';
+}
 
 
+
+//ES6 
+for(const cur of boxArray6){
+    if(cur.className === 'box blue'){
+        continue;
+    }
+    cur.textContent = 'I changed to blue';
+}
+
+//ES5
+var ages = [12, 17, 8, 21,14,11];
+
+
+
+var full = ages.map(function(cur){
+    return cur >= 18;
+});
+
+console.log(full);
+
+console.log(full.indexOf(true));
+
+console.log(ages[full.indexOf(true)]);
+
+
+//ES6
+ 
+
+console.log(ages.findIndex(cur => cur >= 18));
+
+console.log(ages.find (cur => cur >= 18));
+
+*/
+
+// Spread operator
 
 
