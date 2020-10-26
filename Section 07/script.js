49ce1e960e36f744334231d19405ad5631bc5793
HoussemBoubakour <houssemboubakour@gmail.com>
2020-10-26 17:31:21 -0400

    Edit 10/26

--- Section 07/script.js
+++ Section 07/script.js
@@ -249,4 +249,24 @@ console.log(ages.find (cur => cur >= 18));
 
 // Spread operator
 
+var addFourAges = function(a, b, c, d){
+    return a+b+c+d;
+}
 
+
+
+var sum1 = addFourAges(18, 30, 12, 21);
+
+console.log(sum1);
+
+//ES5
+
+var ages = [18, 30, 12, 21];
+
+var sum2 =addFourAges.apply(null, ages); 
+console.log(sum2);
+
+//ES6 
+
+var sum3 = addFourAges(...ages);
+console.log(sum3);
